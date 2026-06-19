90f34f4a2738e59fc61b59fe687c8b2b82ddc977
venkatkoniki-star <konikivenkat438@gmail.com>
2026-06-19 21:56:23 +0530

    add model layer

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,4 +1,71 @@
 package model;
 
 public class Account {
+
+    private int accountId;
+    private int customerId;
+    private String accountNumber;
+    private String accountType;
+    private double balance;
+    private String status;
+
+    public Account() {
+    }
+
+    public Account(int customerId, String accountNumber,
+                   String accountType, double balance, String status) {
+        this.customerId = customerId;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.status = status;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,4 +1,59 @@
 package model;
 
 public class Transaction {
+
+    private int transactionId;
+    private int accountId;
+    private String transactionType;
+    private double amount;
+    private String transactionDate;
+
+    public Transaction() {
+    }
+
+    public Transaction(int accountId, String transactionType, double amount) {
+        this.accountId = accountId;
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(String transactionDate) {
+        this.transactionDate = transactionDate;
+    }
 }
